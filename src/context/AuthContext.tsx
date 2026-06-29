6aac3b0f22825f1cee6fcf5b1c3f1f8794551911
Nguyen Quoc An <nguyenquocan1010@gmail.com>
2026-06-30 05:49:11 +0700

    Redirect to home and reload page on logout

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -78,6 +78,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
         localStorage.removeItem('token');
         setToken(null);
         setUser(null);
+        window.location.href = '/';
     };
 
     const login = async (usernameOrEmail: string, password: string) => {
